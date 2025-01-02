325b3b5f00f9417821d4017825ffb1b6f2d43ae0
lukas <luki.minda@gmail.com>
2025-01-02 18:15:34 +0100

    Add Mui library

--- src/Components/audioList.js
+++ src/Components/audioList.js
@@ -7,17 +7,12 @@ import DataContext from "./DataContext"
 import "./audioList.css"
 
 const audioList = (props) => {
-
     const { state, dispatch, AddProp, oneSong } = props;
-
-
     return (
         <div>
             <div className="album-title-wrapper">
-
                 <div className="Album-artist-image">
-                    <img className="" src="images/beyonce.jfif" ></img>
-
+                    <img className="" src="images/beyonce.jfif"></img>
                 </div>
                 <div className="text-album">
                     <p style={{
